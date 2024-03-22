ffa2cb25223cd48cd76f8f532ba93e160da219da
carloscanoladera <carlos.cano.ladera@outlook.es>
2024-03-22 13:58:43 +0100

    Cerramos semana santa 22-03-2024

--- projectunitefive/src/main/java/iesjandula/projectunitefive/excepciones/biblioteca/ejercicios/ejercicio_1/EnumNota.java
+++
@@ -1,5 +0,0 @@
-package iesjandula.projectunit5.excepciones.ejercicios.ejercicio_1;
-
-public enum EnumNota {
-	NOTA_ALARMA, NOTA_MARCADA;
-}
--- projectunitefive/src/main/java/iesjandula/projectunitefive/excepciones/biblioteca/ejercicios/ejercicio_1/TipoNota.java
+++
@@ -1,6 +0,0 @@
-package iesjandula.projectunit5.excepciones.ejercicios.ejercicio_1;
-
-public interface TipoNota {
-	
-	public EnumNota devuelveTipoNota();
-}
