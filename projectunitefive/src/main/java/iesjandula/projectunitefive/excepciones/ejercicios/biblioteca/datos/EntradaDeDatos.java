18b08f25956822e2da4c454c557a98a62fd10db3
carloscanoladera <carlos.cano.ladera@outlook.es>
2024-04-05 14:35:08 +0200

    05-04-2024 Ejercicios a Hacer

--- projectunitefive/src/main/java/iesjandula/projectunitefive/excepciones/ejercicios/biblioteca/datos/EntradaDeDatos.java
+++ projectunitefive/src/main/java/iesjandula/projectunitefive/excepciones/ejercicios/biblioteca/datos/EntradaDeDatos.java
@@ -255,6 +255,7 @@ public class EntradaDeDatos {
 		cadena = cadena.toLowerCase();
 		cadena = cadena.substring(0, 1).toUpperCase() + cadena.substring(1, cadena.length() - 1);
 
+		
 		return cadena;
 
 	}
